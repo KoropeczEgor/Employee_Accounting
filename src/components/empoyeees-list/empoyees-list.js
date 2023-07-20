9406d985153ec1affaa0602bdcf16cb08aac069a
Egor <koropeczegor@gmail.com>
2023-07-20 16:18:15 +0300

    initial module

---
+++ src/components/empoyeees-list/empoyees-list.js
@@ -0,0 +1,15 @@
+import EmployeesListItem from "../employees-list-item/employees-list-item";
+
+import "./empoyees-list.css";
+
+const EmployeesList = ({ data }) => {
+  const elements = data.map((item) => {
+    const { id, ...itemProps } = item;
+    //этот способ позволяет развернуть массив данных
+    return <EmployeesListItem key={id} {...itemProps} />;
+    // <EmployeesListItem name={item.name} salary={item.salary} />;
+  });
+  return <ul className="app-list list-group">{elements}</ul>;
+};
+
+export default EmployeesList;
