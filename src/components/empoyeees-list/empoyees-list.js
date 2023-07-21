07b9fe809bf57991b29a5a370055ec3875535264
Egor <koropeczegor@gmail.com>
2023-07-21 11:04:04 +0300

    optimization, state changes

--- src/components/empoyeees-list/empoyees-list.js
+++ src/components/empoyeees-list/empoyees-list.js
@@ -2,7 +2,7 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 
 import "./empoyees-list.css";
 
-const EmployeesList = ({ data, onDelete }) => {
+const EmployeesList = ({ data, onDelete, onToggleProp }) => {
   const elements = data.map((item) => {
     const { id, ...itemProps } = item;
     //этот способ позволяет развернуть массив данных
@@ -11,6 +11,9 @@ const EmployeesList = ({ data, onDelete }) => {
         key={id}
         {...itemProps}
         onDelete={() => onDelete(id)}
+        onToggleProp={(e) =>
+          onToggleProp(id, e.currentTarget.getAttribute("data-toggle"))
+        }
       />
     );
     // <EmployeesListItem name={item.name} salary={item.salary} />;
